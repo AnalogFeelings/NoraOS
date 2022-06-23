be883b826e10ea13a9baaa9158f498240434fb1b
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-06-23 22:14:57 +0200

    [KERNEL] Improve bugcheck formatting, use UINTs instead of ULONGs because ULONGs are excessively large.

--- include/ke/ke.h
+++ include/ke/ke.h
@@ -18,5 +18,5 @@ VOID *KiGetStivaleTag(struct stivale2_struct *Stivale2Struct, ULONG64 Id);
 INLINE VOID KeAcquireSpinlock(PKSPIN_LOCK Lock);
 INLINE VOID KeDropSpinlock(PKSPIN_LOCK Lock);
 
-VOID KeBugCheckEx(ULONG64 Error, ULONG64 Code0, ULONG64 Code1, ULONG64 Code2, ULONG64 Code3);
+VOID KeBugCheckEx(UINT Error, UINT Code0, UINT Code1, UINT Code2, UINT Code3);
 #define KeBugCheck(A) KeBugCheckEx(A, 0, 0, 0, 0);
--- src/ke/bug.c
+++ src/ke/bug.c
@@ -2,14 +2,17 @@
 #include <rtl/debug.h>
 #include <kdcom/kdcom.h>
 
-VOID KeBugCheckEx(ULONG64 Error, ULONG64 Code0, ULONG64 Code1, ULONG64 Code2, ULONG64 Code3) {
+VOID KeBugCheckEx(UINT Error, UINT Code0, UINT Code1, UINT Code2, UINT Code3) {
 	KiInterruptDisable;
 
-	RtlDebugPrint("\n!==== BUGCHECK ====!\nThe system has halted to prevent further corruption.\n");
-	KdPrintFormat("\n!==== BUGCHECK ====!\nThe system has halted to prevent further corruption.\n");
+	RtlDebugPrint("\n[==================== BUGCHECK ====================]\n");
+    RtlDebugPrint("The system has halted to prevent further corruption.\n");
+    
+	KdPrintFormat("\n[==================== BUGCHECK ====================]\n");
+    KdPrintFormat("The system has halted to prevent further corruption.\n");
 
-	RtlDebugPrint("\n*** STOP:(0x%p)(0x%p:0x%p:0x%p:0x%p)\n", Error, Code0, Code1, Code2, Code3);
-	KdPrintFormat("\n*** STOP:(0x%p)(0x%p:0x%p:0x%p:0x%p)\n", Error, Code0, Code1, Code2, Code3);
+	RtlDebugPrint("\n*** STOP: 0x%p (0x%p:0x%p:0x%p:0x%p)\n", Error, Code0, Code1, Code2, Code3);
+	KdPrintFormat("\n*** STOP: 0x%p (0x%p:0x%p:0x%p:0x%p)\n", Error, Code0, Code1, Code2, Code3);
 
 	KiProcessorHalt;
 	__builtin_unreachable();
